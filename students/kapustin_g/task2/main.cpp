54b2e42c2bbd97e274bebbbae3b7103b0339f175
Grigory Kapustin <79307199926@yandex.ru>
2018-03-02 15:38:13 +0300

    Добавил только свою папку2

--- students/kapustin_g/task2/main.cpp
+++ students/kapustin_g/task2/main.cpp
@@ -191,7 +191,7 @@ int main()
     cin >> size;
     if (size > 1 && size < 8)
     {
-        matrix A(size);
+        matrix A(size); //
         matrix B(size);
         for (int i = 0; i < size; i++)
         {
