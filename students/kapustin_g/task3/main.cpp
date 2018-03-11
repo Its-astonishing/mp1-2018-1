a088f72955287b18d8206542b63587d9db300d15
Grigory Kapustin <79307199926@yandex.ru>
2018-03-11 21:50:34 +0300

    Переделано с использованием string

--- students/kapustin_g/task3/main.cpp
+++ students/kapustin_g/task3/main.cpp
@@ -1,57 +1,46 @@
 #include <iostream>
+#include <conio.h>
+#include <string>
 #include <windows.h>
 using namespace std;
+
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 class textEditor
 {
 private:
-    char *string;
-    int length;
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    string line;
     COORD position;
+    size_t len;
 public:
-    textEditor(int _length, short _x, short _y):
-        length(_length)
-    {
-        position.X = _x;
-        position.Y = _y;
-        string = new char[length];
-    }
-    ~textEditor()
-    {
-        delete string;
-    }
-    char *getString()
-    {
-        return string;
-    }
-    void setLength(int _length)
-    {
-        length = _length;
-    }
-    int getLength()
-    {
-        return length;
-    }
-    void setCursor(short _x, short _y)
+    textEditor(size_t _len=0, short _x = 1, short _y = 1):
+        len(_len)
     {
         position.X = _x;
         position.Y = _y;
     }
-    void putCursor()
+    textEditor(COORD _position):
+        position(_position) 
     {
-        SetConsoleCursorPosition(hConsole, position);
     }
-    void write()
+    textEditor(textEditor& m)
     {
-        putCursor();
-        cin >> string;
+        line = m.line;
+        len = size(line);
+    }
+    string getLine()
+    {
+        return line;
+    }
+    size_t getLength()
+    {
+        return len;
     }
     COORD getPos()
     {
         return position;
     }
-    short getX() 
+    short getX()
     {
         return position.X;
     }
@@ -59,37 +48,97 @@ public:
     {
         return position.Y;
     }
-    friend ostream& operator<<(ostream& os, const textEditor & dt);
+    void setCursor(short _x, short _y)
+    {
+        position.X = _x;
+        position.Y = _y;
+    }
+    void setCursorCoord(COORD p)
+    {
+        position = p;
+    }
+    void putCursor()
+    {
+        SetConsoleCursorPosition(hConsole, position);
+    }
+    void writeLine(string _line)
+    {
+        line = _line;
+        len = size(line);
+    }
+    void writeTillLen(string _line) //записать в поле ввода символов не больше, чем задано в длине поля
+    {
+            line = _line;
+            for (size_t i = size(line); i > len; i--)
+                line.pop_back();
+    }
+    void setLen(unsigned _len)
+    {
+        len = _len;
+            for (size_t i = size(line); i > len; i--)
+                line.pop_back();
+    }
 };
-ostream& operator<<(ostream& os, const textEditor & dt)
-{
-    os << dt.string;
-    return os;
-}
+
 int main()
 {
-    bool menu;
-    textEditor m(10,15,15);
-   /* do
+    setlocale(LC_ALL, "Russian");
+    int tmp;
+    COORD coord;
+    string buf;
+    bool menu=1;
+    textEditor m(0,10,10);
+    while (menu)
     {
         system("cls");
-        cout << "Set positiont\t\t\t---1"
-            << "Input text\t\t\t---2"
-            << "Show textt\t\t\t---3"
-            << "Get positiont\t\t\t---4";
-
-
-    } while (menu); */
-    m.putCursor();
-    m.write();
-    system("cls");
-    m.putCursor();
-    cout << m;
-    system("pause");
+        cout << "Установить координаты(x,y)  \t\t\t\t---1" << endl
+            << "Ввести текст                 \t\t\t\t---2" << endl
+            << "Ввести текст (не больше заданной длины)\t\t\t---3" << endl
+            << "Установить новую длину       \t\t\t\t---4" << endl
+            << "Показать текст               \t\t\t\t---5" << endl
+            << "Показать позицию             \t\t\t\t---6" << endl
+            << "Показать длину               \t\t\t\t---7" << endl
+            << "Выход                        \t\t\t\t---8" << endl;
+        cin >> tmp;
+        switch (tmp)
+        {
+        case 1:
+            cin >> coord.X >> coord.Y;
+            m.setCursorCoord(coord);
+            break;
+        case 2:
+            m.putCursor();
+            cin.ignore();
+            getline(cin, buf);
+            m.writeLine(buf);
+            break;
+        case 3:
+            m.putCursor();
+            cin.ignore();
+            getline(cin, buf);
+            m.writeTillLen(buf); 
+            break;
+        case 4:
+            cin >> tmp;
+            m.setLen(tmp);
+            break;
+        case 5:
+            m.putCursor();
+            cout << m.getLine();
+            _getch();
+            break;
+        case 6:
+            cout << "(" << m.getX() << "," << m.getY() << ")";
+            _getch();
+            break;
+        case 7:
+            cout << m.getLength();
+            _getch();
+            break;
+        case 8:
+            menu = 0;
+            break;
+        }
+    }
     return 0;
 }
-/*HANDLE hConsole1 = GetStdHandle(STD_OUTPUT_HANDLE);
-COORD position1;
-position1.X = 10;
-position1.Y = 10;
-SetConsoleCursorPosition(hConsole1, position1);*/
